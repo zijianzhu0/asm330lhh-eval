57ffa4820bdc8b7a596633e236adcba407b4fcdf
Zijian Zhu <45644409+Petelr@users.noreply.github.com>
2025-02-27 08:40:51 -0800

    added asm330lhh example

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -15,6 +15,13 @@
   *
   ******************************************************************************
   */
+#define STEVAL_MKI109V3
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+#endif
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,7 +29,17 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include "asm330lhh_reg.h"
 
+#if defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_spi.h"
+#include "stm32f4xx_hal_tim.h"
+#endif
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -37,27 +54,51 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+#define    BOOT_TIME            10 //ms
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi2;
 
-/* USER CODE BEGIN PV */
+TIM_HandleTypeDef htim3;
 
+/* USER CODE BEGIN PV */
+static int16_t data_raw_acceleration[3];
+static int16_t data_raw_angular_rate[3];
+static int16_t data_raw_temperature;
+static float_t acceleration_mg[3];
+static float_t angular_rate_mdps[3];
+static float_t temperature_degC;
+static uint8_t whoamI, rst;
+static uint8_t tx_buffer[1000];
+static GPIO_TypeDef *CS_up_GPIO_Port = GPIOB; // Variable to store the GPIO port
+static uint16_t CS_up_Pin = GPIO_PIN_12;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI2_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
 /* USER CODE END 0 */
 
 /**
@@ -91,8 +132,46 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI2_Init();
   MX_USB_DEVICE_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
+  /* Initialize mems driver interface */
+    stmdev_ctx_t dev_ctx;
+    dev_ctx.write_reg = platform_write;
+    dev_ctx.read_reg = platform_read;
+    dev_ctx.mdelay = platform_delay;
+    dev_ctx.handle = &SENSOR_BUS;
+    /* Init test platform */
+    platform_init();
+    /* Wait sensor boot time */
+    platform_delay(BOOT_TIME);
+    /* Check device ID */
+    asm330lhh_device_id_get(&dev_ctx, &whoamI);
 
+    if (whoamI != ASM330LHH_ID)
+      while (1);
+
+    /* Restore default configuration */
+    asm330lhh_reset_set(&dev_ctx, PROPERTY_ENABLE);
+
+    do {
+      asm330lhh_reset_get(&dev_ctx, &rst);
+    } while (rst);
+
+    /* Start device configuration. */
+    asm330lhh_device_conf_set(&dev_ctx, PROPERTY_ENABLE);
+    /* Enable Block Data Update */
+    asm330lhh_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+    /* Set Output Data Rate */
+    asm330lhh_xl_data_rate_set(&dev_ctx, ASM330LHH_XL_ODR_12Hz5);
+    asm330lhh_gy_data_rate_set(&dev_ctx, ASM330LHH_GY_ODR_12Hz5);
+    /* Set full scale */
+    asm330lhh_xl_full_scale_set(&dev_ctx, ASM330LHH_2g);
+    asm330lhh_gy_full_scale_set(&dev_ctx, ASM330LHH_2000dps);
+    /* Configure filtering chain(No aux interface)
+     * Accelerometer - LPF1 + LPF2 path
+     */
+    asm330lhh_xl_hp_path_on_out_set(&dev_ctx, ASM330LHH_LP_ODR_DIV_100);
+    asm330lhh_xl_filter_lp2_set(&dev_ctx, PROPERTY_ENABLE);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -102,6 +181,56 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  uint8_t reg;
+	      /* Read output only if new xl value is available */
+	      asm330lhh_xl_flag_data_ready_get(&dev_ctx, &reg);
+
+	      if (reg) {
+	        /* Read acceleration field data */
+	        memset(data_raw_acceleration, 0x00, 3 * sizeof(int16_t));
+	        asm330lhh_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+	        acceleration_mg[0] =
+	          asm330lhh_from_fs2g_to_mg(data_raw_acceleration[0]);
+	        acceleration_mg[1] =
+	          asm330lhh_from_fs2g_to_mg(data_raw_acceleration[1]);
+	        acceleration_mg[2] =
+	          asm330lhh_from_fs2g_to_mg(data_raw_acceleration[2]);
+	        snprintf((char *)tx_buffer, sizeof(tx_buffer),
+	                "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
+	                acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
+	        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+	      }
+
+	      asm330lhh_gy_flag_data_ready_get(&dev_ctx, &reg);
+
+	      if (reg) {
+	        /* Read angular rate field data */
+	        memset(data_raw_angular_rate, 0x00, 3 * sizeof(int16_t));
+	        asm330lhh_angular_rate_raw_get(&dev_ctx, data_raw_angular_rate);
+	        angular_rate_mdps[0] =
+	          asm330lhh_from_fs2000dps_to_mdps(data_raw_angular_rate[0]);
+	        angular_rate_mdps[1] =
+	          asm330lhh_from_fs2000dps_to_mdps(data_raw_angular_rate[1]);
+	        angular_rate_mdps[2] =
+	          asm330lhh_from_fs2000dps_to_mdps(data_raw_angular_rate[2]);
+	        snprintf((char *)tx_buffer, sizeof(tx_buffer),
+	                "Angular rate [mdps]:%4.2f\t%4.2f\t%4.2f\r\n",
+	                angular_rate_mdps[0], angular_rate_mdps[1], angular_rate_mdps[2]);
+	        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+	      }
+
+	      asm330lhh_temp_flag_data_ready_get(&dev_ctx, &reg);
+
+	      if (reg) {
+	        /* Read temperature data */
+	        memset(&data_raw_temperature, 0x00, sizeof(int16_t));
+	        asm330lhh_temperature_raw_get(&dev_ctx, &data_raw_temperature);
+	        temperature_degC = asm330lhh_from_lsb_to_celsius(
+	                             data_raw_temperature);
+	        snprintf((char *)tx_buffer, sizeof(tx_buffer),
+	                "Temperature [degC]:%6.2f\r\n", temperature_degC);
+	        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+	      }
   }
   /* USER CODE END 3 */
 }
@@ -169,11 +298,11 @@ static void MX_SPI2_Init(void)
   /* SPI2 parameter configuration*/
   hspi2.Instance = SPI2;
   hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_1LINE;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
   hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi2.Init.CLKPolarity = SPI_POLARITY_HIGH;
   hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.NSS = SPI_NSS_HARD_OUTPUT;
   hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
   hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
@@ -186,6 +315,55 @@ static void MX_SPI2_Init(void)
   /* USER CODE BEGIN SPI2_Init 2 */
 
   /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sSlaveConfig.InputTrigger = TIM_TS_ETRF;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_NONINVERTED;
+  sSlaveConfig.TriggerPrescaler = TIM_TRIGGERPRESCALER_DIV1;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim3, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
 
 }
 
@@ -205,12 +383,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PE7 PE8 PE9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  /*Configure GPIO pins : PE8 PE9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -221,7 +400,83 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+  return 0;
+}
 
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+  return 0;
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+  CDC_Transmit_FS(tx_buffer, len);
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+  HAL_Delay(ms);
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
 /* USER CODE END 4 */
 
 /**
